fe40cfedaa177b4613e7a7f526414bf52e7ab921
Jasmine Mary <jasminemary630@gmail.com>
2023-07-19 18:58:36 +0530

    Subtraction feature added

--- model/com/dev/Calculator.java
+++ model/com/dev/Calculator.java
@@ -5,5 +5,10 @@ public class Calculator {
         int sum = num1 + num2;
         return sum;
     }
+
+    public int sub(int num1, int num2){
+        int sub = num1 - num2;
+        return sub;
+    }
 }
 
--- model/com/dev/Main.java
+++ model/com/dev/Main.java
@@ -3,7 +3,9 @@ package com.dev;
 public class Main {
     public static void main(String[] args){
         Calculator calculator = new Calculator();
-        int c = calculator.add(5, 4);
-        System.out.println(c);
+        int add = calculator.add(5, 4);
+        System.out.println("The sum of two number is: "+add);
+        int sub = calculator.sub(5, 1);
+        System.out.println("The subtraction of two number is: "+sub);
     }
 }
