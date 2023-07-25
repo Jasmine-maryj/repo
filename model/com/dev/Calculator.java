8f6c396316acdbbbba79de28c5e3c987d1304dc5
Jasmine Mary <jasminemary630@gmail.com>
2023-07-25 22:09:15 +0530

    Completed task

--- model/com/dev/Calculator.java
+++ model/com/dev/Calculator.java
@@ -1,18 +1,29 @@
 package com.dev;
 
+import java.util.Scanner;
+
 public class Calculator {
-    public int add(int num1, int num2){
-        int sum = num1 + num2;
-        return sum;
-    }
-
-    public int sub(int num1, int num2){
-        int sub = num1 - num2;
-        return sub;
-    }
-
-    public int mul(int num1, int num2){
-        return num1 * num2;
+    public int calculator(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two number: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Enter the operation you want to perform(+, -, /, *)");
+        String ch = sc.next();
+        int result = 0;
+        switch(ch){
+            case "+": result = num1 + num2;
+                        break;
+            case "-": result = num1 - num2;
+                        break;
+            case "*": result = num1 * num2;
+                        break;
+            case "/": result = num1 / num2;
+                        break;
+            default: 
+                System.out.println("invalid operation");
+        }
+        return result;
     }
 }
 
--- model/com/dev/Main.java
+++ model/com/dev/Main.java
@@ -3,9 +3,7 @@ package com.dev;
 public class Main {
     public static void main(String[] args){
         Calculator calculator = new Calculator();
-        int add = calculator.add(5, 4);
-        System.out.println("The sum of two number is: "+add);
-        int sub = calculator.sub(5, 1);
-        System.out.println("The subtraction of two number is: "+sub);
+        int result = calculator.calculator();
+        System.out.println(result);
     }
 }
